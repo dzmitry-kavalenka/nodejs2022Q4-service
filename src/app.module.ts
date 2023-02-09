0c7bbfefe9106fda2967f470182bdec24d460ca0
dzmitry-kavalenka <dzmitriy.kavalenka@gmail.com>
2023-02-09 19:25:09 +0100

    integrate typeorm with postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { InMemoryDBModule } from '@nestjs-addons/in-memory-db';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
@@ -8,6 +9,17 @@ import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      migrations: [__dirname + '../migrations/**/*{.ts,.js}'],
+      synchronize: true,
+    }),
     InMemoryDBModule.forRoot({}),
     UserModule,
     ArtistModule,
