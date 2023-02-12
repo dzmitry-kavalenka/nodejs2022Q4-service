3a60d33352cbff3e338a1fd88af3edd8fd689d4e
dzmitry-kavalenka <dzmitriy.kavalenka@gmail.com>
2023-02-12 20:03:14 +0100

    ref: update migrations

--- migrations/1676228534819-CreateTracks.ts
+++ migrations/1676228534819-CreateTracks.ts
@@ -1,13 +1,11 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateTracks1676227071346 implements MigrationInterface {
-  name = 'CreateTracks1676227071346';
+export class CreateTracks1676228534819 implements MigrationInterface {
+  name = 'CreateTracks1676228534819';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "tracks" ADD "artistIdId" uuid`);
-    await queryRunner.query(`ALTER TABLE "tracks" ADD "albumIdId" uuid`);
     await queryRunner.query(
-      `ALTER TABLE "albums" ADD CONSTRAINT "FK_f74d24b7a35551b0c9cc650be11" FOREIGN KEY ("artistIdId") REFERENCES "artists"("id") ON DELETE SET NULL ON UPDATE NO ACTION`,
+      `CREATE TABLE "tracks" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "artistId" uuid, "albumId" uuid, "duration" integer NOT NULL, "artistIdId" uuid, "albumIdId" uuid, CONSTRAINT "PK_242a37ffc7870380f0e611986e8" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `ALTER TABLE "tracks" ADD CONSTRAINT "FK_9b1a336159819e6518395fa0a18" FOREIGN KEY ("artistIdId") REFERENCES "artists"("id") ON DELETE SET NULL ON UPDATE NO ACTION`,
@@ -24,11 +22,6 @@ export class CreateTracks1676227071346 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "tracks" DROP CONSTRAINT "FK_9b1a336159819e6518395fa0a18"`,
     );
-    await queryRunner.query(
-      `ALTER TABLE "albums" DROP CONSTRAINT "FK_f74d24b7a35551b0c9cc650be11"`,
-    );
-    await queryRunner.query(`ALTER TABLE "tracks" DROP COLUMN "albumIdId"`);
-    await queryRunner.query(`ALTER TABLE "tracks" DROP COLUMN "artistIdId"`);
     await queryRunner.query(`DROP TABLE "tracks"`);
   }
 }
