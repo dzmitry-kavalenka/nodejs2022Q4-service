24578fb14b0f4d62c6f223c68862da65524fe69d
dzmitry-kavalenka <dzmitriy.kavalenka@gmail.com>
2023-02-12 15:00:46 +0100

    feat: integrate user module with typeorm

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,11 @@
-import { InMemoryDBModule } from '@nestjs-addons/in-memory-db';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
+import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [InMemoryDBModule.forFeature('user')],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,59 +1,50 @@
-import { InMemoryDBService } from '@nestjs-addons/in-memory-db';
 import {
   ForbiddenException,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
 import { compare } from 'bcrypt';
-import { v4 as uuidV4 } from 'uuid';
+import { Repository } from 'typeorm';
 import * as INFO from '../constants';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UpdatePasswordDto } from './dto/updatePassword.dto';
 import { UserEntity } from './user.entity';
 import { INCORRECT_PASSWORD_ERROR } from './constants';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: InMemoryDBService<UserEntity>) {}
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
-  async getAll(): Promise<UserEntity[]> {
-    const users = this.userRepository.getAll();
-
-    return users.map((user) => this.buildResponse(user));
+  getAll(): Promise<UserEntity[]> {
+    return this.userRepository.find();
   }
 
   async getById(id: string): Promise<UserEntity> {
-    const user = this.userRepository.get(id);
+    const user = await this.userRepository.findOneBy({ id });
 
     if (!user) {
       throw new NotFoundException(INFO.NOT_FOUND_ERROR);
     }
 
-    return this.buildResponse(user);
+    return user;
   }
 
-  async create(body: CreateUserDto): Promise<UserEntity> {
-    const timestamp = Date.now();
+  create(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const newUser = new UserEntity();
+    Object.assign(newUser, createUserDto);
 
-    const newUser = new UserEntity({
-      id: uuidV4(),
-      login: body.login,
-      version: 1,
-      createdAt: timestamp,
-      updatedAt: timestamp,
-    });
-
-    await newUser.hashPassword(body.password);
-
-    const user = this.userRepository.create(newUser);
-    return this.buildResponse(user);
+    return this.userRepository.save(newUser);
   }
 
   async updatePassword(
     id: string,
     body: UpdatePasswordDto,
   ): Promise<UserEntity> {
-    const user = this.userRepository.get(id);
+    const user = await this.userRepository.findOneBy({ id });
 
     if (!user) {
       throw new NotFoundException(INFO.NOT_FOUND_ERROR);
@@ -65,25 +56,17 @@ export class UserService {
       throw new ForbiddenException(INCORRECT_PASSWORD_ERROR);
     }
 
-    const updatedUser = new UserEntity(user);
-    await updatedUser.hashPassword(body.newPassword);
-    updatedUser.updateVersion();
-
-    this.userRepository.update(updatedUser);
-    return updatedUser;
+    Object.assign(user, { password: body.newPassword });
+    return this.userRepository.save(user);
   }
 
   async delete(id: string) {
-    const user = this.userRepository.get(id);
+    const user = await this.userRepository.findOneBy({ id });
 
     if (!user) {
       throw new NotFoundException(INFO.NOT_FOUND_ERROR);
     }
 
     return this.userRepository.delete(id);
-  }
-
-  buildResponse(user: UserEntity) {
-    return new UserEntity(user);
   }
 }
